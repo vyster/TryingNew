44e6bd54f5ad14d2e958bcb1ab6caddc2323bf83
Vyom Agarwal <vyom.agarwal.met16@iitbhu.ac.in>
2024-09-22 19:00:25 +0530

    singleton

--- singleton.cpp
+++ singleton.cpp
@@ -3,21 +3,22 @@
 
 class Singleton {
 public:
-  Singleton(const Singleton&) = delete;
-  Singleton& operator=(const Singleton&) = delete;
+  Singleton(const Singleton&) = delete; //copy constructor deleted
+  Singleton& operator=(const Singleton&) = delete;//copy overloading not allowed
 
   static Singleton* instance() {
     if (!instance_) {
       instance_ = new Singleton();
-      std::cout << "First Time" << std::endl;
+      std::cout << "1st Time" << std::endl;
+      return instance_;
     }
     std::cout << "Next Time" << std::endl;
     return instance_;
   }
 
-  void checkSingleton() { std::cout << "Singleton has been created" << std::endl; }
+  void checkSingleton() { std::cout << "Singleton call" << std::endl; }
 private:
-  Singleton() { std::cout << "Singleton" << std::endl; }
+  Singleton() { std::cout << "Singleton Constructor" << std::endl; }
   static Singleton* instance_;
 };
 
@@ -30,6 +31,11 @@ int main(int argc, char* argv[]) {
   std::cout << "---------" << std::endl;
   singleton->checkSingleton();
   std::cout << "---------" << std::endl;
-  Singleton *singleton1 = Singleton::instance();
+  singleton->checkSingleton();
+  std::cout << "---------" << std::endl;
+  Singleton *singleton1;
+  std::cout << "---------" << std::endl;
+  singleton1 = Singleton::instance();
+  std::cout << "---------" << std::endl;
   singleton1->checkSingleton();
 }
