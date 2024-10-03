bf5274636bbf297f8e1cce7013d34206c848cae6
Vyom Agarwal <vyom.agarwal.met16@iitbhu.ac.in>
2024-10-03 22:39:41 +0530

    new

--- MemoryAnalysis/StackMemoryManagement.cpp
+++ MemoryAnalysis/StackMemoryManagement.cpp
@@ -5,6 +5,7 @@ void stackExample() {
     int a = 10;          // Allocated on the stack
     double b = 20.5;     // Allocated on the stack
     std::cout << "a: " << a << ", b: " << b << std::endl;
+    
 } // 'a' and 'b' are automatically deallocated here
 
 int main() {
